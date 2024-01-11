a77733047a79545b5112914c455838232b3b3336
Nando <24811313+fzavalia@users.noreply.github.com>
2024-01-11 12:09:54 -0300

    fix: Storage component now deletes expired requests every x seconds

--- src/ports/storage/component.ts
+++ src/ports/storage/component.ts
@@ -1,8 +1,22 @@
 import { IStorageComponent, StorageRequest } from './types'
 
-export function createStorageComponent(): IStorageComponent {
+export function createStorageComponent(args: { clearRequestsInSeconds: number }): IStorageComponent {
   const requests: Record<string, StorageRequest> = {}
   const requestIdsBySocketId: Record<string, string> = {}
+
+  // Clears expired requests every x amount of time.
+  setInterval(() => {
+    for (const key in requests) {
+      if (Object.prototype.hasOwnProperty.call(requests, key)) {
+        const request = requests[key]
+
+        if (request.expiration.getTime() < Date.now()) {
+          delete requests[key]
+          delete requestIdsBySocketId[request.socketId]
+        }
+      }
+    }
+  }, args.clearRequestsInSeconds * 1000)
 
   const getRequest = (requestId: string) => {
     return requests[requestId] ?? null
