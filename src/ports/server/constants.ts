36ba9e51f885e49f1399c5d817daa614fd5d709d
LautaroPetaccio <lausuper@gmail.com>
2025-12-22 12:40:44 +0100

    feat: Add requests and outcome limits

--- src/ports/server/constants.ts
+++ src/ports/server/constants.ts
@@ -1,2 +1,9 @@
 export const METHOD_DCL_PERSONAL_SIGN = 'dcl_personal_sign'
 export const FIFTEEN_MINUTES_IN_MILLISECONDS = 15 * 60 * 1000
+
+// Max length constants for request/outcome validation
+export const MAX_METHOD_LENGTH = 256
+export const MAX_PARAMS_ITEMS = 10
+export const MAX_ERROR_MESSAGE_LENGTH = 10024
+export const MAX_REQUEST_ID_LENGTH = 36 // UUID length
+export const MAX_BODY_SIZE = '16kb'
